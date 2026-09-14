b7e6c0f7b232b41b989c211578c5c97d4220bf63
Juaomp <Joaopedromachado830@gmail.com>
2026-09-14 15:00:48 -0300

    chore: initial Spring Boot backend structure for MA Machado Nails

---
+++ src/test/java/com/mamachado/nail_studio_api/NailStudioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamachado.nail_studio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailStudioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
